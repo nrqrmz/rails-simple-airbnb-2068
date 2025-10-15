1c3cecc421a0f4251c9a5c91ac4aa652c7724847
Enrique Ramirez <nrqrmz@gmail.com>
2025-10-15 01:55:18 -0600

    add picture_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_15_061354) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_15_073706) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_15_061354) do
     t.integer "number_of_guests"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
   end
 
 end
